4bb399aea4402191202f31422df685c0d514969c
pablo.inchausti <pablo.inchausti@bbva.com>
2017-12-25 22:48:59 -0300

    CentOS Scripts

---
+++ CentOS/tmux.sh
@@ -0,0 +1,17 @@
+# Install tmux on CentOS
+#   Based on:
+#   https://gist.github.com/Root-shady/d48d5282651634f464af
+
+# (1). tmux has a library dependency on libevent which, of course, isn’t installed by default. 
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar xzvf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure && make
+sudo make install
+
+
+# (2). To get and build the latest from version control:
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
