ebfd26d2ca0188ff3e021d5839bee1ab557f0e18
PabloEzequiel <inchausti.prod@gmail.com>
2018-01-07 06:53:54 -0300

    Some shell scripts

---
+++ logstash/scripts/tesis-crbuoy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Start"
+
+MONGODB_HOST=ds163156.mlab.com:63156
+MONGODB_DB=uela
+MONGODB_USER=$MONGODB_MLAB_USER
+MONGODB_PASS=$MONGODB_MLAB_PASS
+
+echo "Step #01 $MONGODB_MLAB_USER - $MONGODB_MLAB_PASS "
+
+
+# mongoimport -h $MONGODB_HOST -d $MONGODB_DB   \
+#             -u $MONGODB_USER -p $MONGODB_PASS \
+#             -c crbuoy-csv  --type csv         \
+#             --headerline --file ../files/crbuoy2015-out.csv
+
+
