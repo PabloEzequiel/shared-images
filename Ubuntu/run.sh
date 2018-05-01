15a9f5b1c5948436b53f50c19512c519dc380082
pablo.inchausti <pablo.inchausti@bbva.com>
2018-05-01 15:44:14 -0300

    command ubuntu resolution

--- Ubuntu/run.sh
+++ Ubuntu/run.sh
@@ -1,1 +1,2 @@
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/puppet-autoinstall/icaclient_12.0.0_i386.deb
+# wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/puppet-autoinstall/icaclient_12.0.0_i386.deb
+sudo xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
