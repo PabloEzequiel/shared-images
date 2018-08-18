53a8db36f0e6b3112a670dd8635cc2686add7980
PabloEzequiel <inchausti.prod@gmail.com>
2018-08-18 12:23:04 -0300

    Common Bash for Ubuntu Servers

---
+++ Ubuntu/new-server.sh
@@ -0,0 +1,10 @@
+# Config for a New Ubuntu Server
+
+# Git Config
+
+## Add git branch name to bash prompt
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
