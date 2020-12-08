62576fdea3ba1179f037b21f7027ae7fae333209
pablo.inchausti <pablo.inchausti@bbva.com>
2020-12-08 11:30:07 -0300

    ID-#01 Add AWS SDK and Presigned URL ~ OK

--- lambda/java-aws-sample/src/main/java/aws/s3sample/GeneratePresignedUrlAndUploadObject.java
+++ lambda/java-aws-sample/src/main/java/aws/s3sample/GeneratePresignedUrlAndUploadObject.java
@@ -3,6 +3,8 @@ package aws.s3sample;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -24,12 +26,25 @@ public class GeneratePresignedUrlAndUploadObject {
 
     public static void main(String[] args) throws IOException {
         Regions clientRegion = Regions.US_EAST_1; // Regions.DEFAULT_REGION;
-        String bucketName = "fruta-web";
-        String objectKey = "miobjeto2";
+        String bucketName = "ninja-presigned-bucket-web";
+        String objectKey  = "ninja-object.txt";
 
         try {
+         
+            // Pablo
+            ProfileCredentialsProvider profile = new ProfileCredentialsProvider();
+
+            AWSCredentials credentials = profile.getCredentials();
+
+            System.out.println("Profile: " + profile.toString());
+            System.out.println("credentials: " + credentials.toString());
+
+            // System.out.println("credentials [AWS Access Key   ]: " + credentials.getAWSAccessKeyId());
+	        // System.out.println("credentials [AWS Access Secret]: " + credentials.getAWSSecretKey());
+	
+
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new ProfileCredentialsProvider())
+                    .withCredentials(profile)
                     .withRegion(clientRegion)
                     .build();
 
@@ -39,41 +54,51 @@ public class GeneratePresignedUrlAndUploadObject {
             expTimeMillis += 1000 * 60 * 60;
             expiration.setTime(expTimeMillis);
 
-            // Generate the pre-signed URL.
+
             System.out.println("Generating pre-signed URL.");
+
             GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, objectKey)
                     .withMethod(HttpMethod.PUT)
                     .withExpiration(expiration);
-            URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+            URL urlPOST = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
 
+            System.out.println("PresignedUrlRequest [POST]: " + urlPOST); 
 
-            System.out.println("PresignedUrlRequest: " + url); 
-
-            // Create the connection and use it to upload the new object using the pre-signed URL.
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Upload the new object using the pre-signed URL.
+            HttpURLConnection connection = (HttpURLConnection) urlPOST.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod("PUT");
             OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-            out.write("This text uploaded as an object via presigned URL.");
+            out.write("Un objeto Ninja, creado via presigned URL.");
             out.close();
 
-            // Check the HTTP response code. To complete the upload and make the object available, 
-            // you must interact with the connection object in some way.
+
+            // Generating pre-signed URL [xGET].
+            System.out.println("Generating pre-signed URL [xGET].");
+            GeneratePresignedUrlRequest generatePresignedUrlRequestGET = new GeneratePresignedUrlRequest(bucketName, objectKey)
+                    .withMethod(HttpMethod.GET)
+                    .withExpiration(expiration);
+            URL urlGET = s3Client.generatePresignedUrl(generatePresignedUrlRequestGET);
+
+            System.out.println("PresignedUrlRequest [GET]: " + urlGET); 
+
+
+            // Check the HTTP response code
             connection.getResponseCode();
             System.out.println("HTTP response code: " + connection.getResponseCode());
 
-            // Check to make sure that the object was uploaded successfully.
+            // Check the object was uploaded successfully.
             S3Object object = s3Client.getObject(bucketName, objectKey);
             System.out.println("Object " + object.getKey() + " created in bucket " + object.getBucketName());
+
         } catch (AmazonServiceException e) {
-            // The call was transmitted successfully, but Amazon S3 couldn't process 
-            // it, so it returned an error response.
             e.printStackTrace();
         } catch (SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client  
-            // couldn't parse the response from Amazon S3.
             e.printStackTrace();
         }
+
+
+
     }
 }
 
